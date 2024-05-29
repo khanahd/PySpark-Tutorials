3c812de7ba96c834f1fb7e267128b13ff69227fa
khanahd <163975544+khanahd@users.noreply.github.com>
2024-05-29 12:39:14 +0530

    Pyspark_tutorial.py

--- Pyspark_tutorial.py
+++ Pyspark_tutorial.py
@@ -1,5 +1,5 @@
 '''
-Tutorial 1
+[Tutorial 1]
 Installing the PySpark and basic operations on PySpark
 '''
 !pip install pyspark
@@ -22,3 +22,19 @@ df_pyspark.head()
 # information regarding the columns
       # will give information on the column data types
 df_pyspark.printSchema()  
+
+# =======================================================================
+'''
+[Tutorial 2]  - [Part 1]
+Covers data preprocessing
+use a dataset saved in the same location as the lab
+'''
+# Step1: start a new spark session with name 'spark'
+
+# Step2: reading the dataset, used a csv file for example
+
+df_pyspark = spark.read.option('header','true').csv('filename', inferSchem=True)
+df_pyspark.show()
+
+# checking the schema, gives the information on the columns
+df_pyspark.printSchema()
