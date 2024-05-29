1e03be9692d17aec828e17f9cb243b22e6e500f5
khanahd <163975544+khanahd@users.noreply.github.com>
2024-05-29 12:49:19 +0530

    Pyspark_tutorial.py

--- Pyspark_tutorial.py
+++ Pyspark_tutorial.py
@@ -1,6 +1,9 @@
 '''
 [Tutorial 1]
 Installing the PySpark and basic operations on PySpark
+
+? what is a DataFrame:
+A dataframe is a data structure constructed with rows and columns, similar to a database or Excel spreadsheet
 '''
 !pip install pyspark
 # Install java on your system too for pyspark to work
@@ -32,9 +35,18 @@ use a dataset saved in the same location as the lab
 # Step1: start a new spark session with name 'spark'
 
 # Step2: reading the dataset, used a csv file for example
-
-df_pyspark = spark.read.option('header','true').csv('filename', inferSchem=True)
+df_pyspark = spark.read.option('header','true').csv('filename',header=True, inferSchem=True)
 df_pyspark.show()
+df_pyspark.dtypes()
+df_pyspark.describe().show()
 
 # checking the schema, gives the information on the columns
 df_pyspark.printSchema()
+df_pyspark.columns            # getting the column names
+
+# Getting a particular column from the df
+df.select('colname')            # output is basic info on the column
+df.select('colname').show()
+df.select(['colname1','colname2'])      # selecting multiple columns from the dataframe
+df['colname'] # will only understand that there is a column with the given name and would not fetch any information from the column
+
