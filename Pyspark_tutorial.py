5e2050ecb9c592f9af7f5ed62b31d320d161f8ac
khanahd <163975544+khanahd@users.noreply.github.com>
2024-05-29 12:28:39 +0530

    Pyspark_tutorial.py

--- Pyspark_tutorial.py
+++ Pyspark_tutorial.py
@@ -1,1 +1,24 @@
+'''
+Tutorial 1
+Installing the PySpark and basic operations on PySpark
+'''
+!pip install pyspark
+# Install java on your system too for pyspark to work
 
+import pyspark
+from pyspark.sql import SparkSession
+
+spark = SparkSession.buidler.appName('Practice').getOrCreate()
+spark     # show the session
+
+# reading a file from spark
+df_pyspark = spark.read.csv('filename')
+      # or to include headers from the file itself use below;
+      # df_pyspark = spark.read.option('header','true').csv('fielname')
+df_pyspark.show()
+# type(df_pyspark)    # output to be 'pyspark.sql.dataframe.DataFrame
+df_pyspark.head()
+
+# information regarding the columns
+      # will give information on the column data types
+df_pyspark.printSchema()  
