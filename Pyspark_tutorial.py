7533bc88ddd57e821597249acbb84a3d457b64cb
khanahd <163975544+khanahd@users.noreply.github.com>
2024-05-30 22:27:04 +0530

    Pyspark_tutorial.py

--- Pyspark_tutorial.py
+++ Pyspark_tutorial.py
@@ -50,3 +50,50 @@ df.select('colname').show()
 df.select(['colname1','colname2'])      # selecting multiple columns from the dataframe
 df['colname'] # will only understand that there is a column with the given name and would not fetch any information from the column
 
+# Adding and Droping columns
+'''
+These are not inplace operations, these changes need to be assigned to a variable
+'''
+# 1 Adding
+df_new = df_pyspark.withColumn('Col_New',df_pyspark['col_old']+2)
+
+# 2 Dropping
+df_new = df_pyspark.drop('col_name').show()
+
+# 3 Renaming a column
+df_new = df_pyspark.withColumnRenamed('col_old_name','col_new_name')
+df_new.show()
+
+'''
+Tutorial Part 3
+'''
+
+# this feature below drops the complete where it find the null values are present
+df_pyspark.na.drop()
+
+# how = any/all
+df.na.drop(how='all')      # the row gets dropped only when all column values are null
+df.na.drop(how='any')      # the row gets dropped only when any column values are null
+
+# threshold
+df.na.drop(how='any',thresh=2)
+# row gets dropped when the row has non null values less than thresh
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
